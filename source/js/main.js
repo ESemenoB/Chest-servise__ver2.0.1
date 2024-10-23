6c938d52f6186d05222201dce89e9d445837a934
ESemenoB <semen_es@mail.ru>
2024-10-23 21:19:23 +0600

    Убирает мусор

--- source/js/main.js
+++ source/js/main.js
@@ -20,22 +20,3 @@ menuToggle.addEventListener("click", (evt) => {
   evt.target.closest(".menu").classList.toggle("menu--open");
   closeAllSubmenuItems();
 });
-
-// // Слайдер
-// let currentSlideIndex = 0;
-// const heroSection = document.querySelector(".hero");
-// const sliderItems = document.querySelectorAll(".slider__item");
-// const sliderControls = document.querySelectorAll(".slider__radio");
-
-// const changeCurrentSlideTo = (index) => {
-//   sliderItems[currentSlideIndex].classList.remove("slider__item--active");
-//   currentSlideIndex = index;
-//   sliderItems[index].classList.add("slider__item--active");
-//   heroSection.dataset.slide = `${index + 1}`;
-// };
-
-// sliderControls.forEach((element, index) => {
-//   element.addEventListener("change", () => {
-//     changeCurrentSlideTo(index);
-//   });
-// });
