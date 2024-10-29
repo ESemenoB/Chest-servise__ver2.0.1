ba7f860d8ff699778154a79514df2ab996f5520b
ESemenoB <semen_es@mail.ru>
2024-10-29 22:18:03 +0600

    Добавляет meta

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,8 +1,3 @@
-////////////// swiper
-
-// import Swiper from "swiper";
-// import "swiper/swiper-bundle.css";
-
 const swiper = new Swiper(".swiper-container", {
   // Опции Swiper
   slidesPerView: 1,
@@ -18,16 +13,4 @@ const swiper = new Swiper(".swiper-container", {
   },
 
   loop: true, // Зацикленное переключение слайдов
-  //   breakpoints: {
-  //     // Когда ширина окна 768px и больше (обычное разрешение для планшетов)
-  //     768: {
-  //       slidesPerView: 2, // Показывать 2 слайда
-  //       spaceBetween: 10, // Увеличить расстояние между слайдами
-  //     },
-  //     // Можно задать другие разрешения
-  //     1024: {
-  //       slidesPerView: 3, // Например, 3 слайда на более широких экранах
-  //       spaceBetween: 30,
-  //     },
-  //   },
 });
